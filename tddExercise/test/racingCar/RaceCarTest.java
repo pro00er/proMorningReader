fe27f3c4764ac00672b4e2e9e8c208f1dab3d455
ohahohah <ohahohah.dev@gmail.com>
2018-09-10 09:47:31 +0900

    자동차 경주게임 구현을 위한 빈 클래스 파일 생성

---
+++ tddExercise/test/racingCar/RaceCarTest.java
@@ -0,0 +1,15 @@
+package racingCar;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class RaceCarTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
