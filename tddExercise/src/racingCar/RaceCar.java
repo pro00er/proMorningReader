70c0b94cc5fbb4e9853a1b15f5570a8358a50f74
ohahohah <ohahohah.dev@gmail.com>
2019-01-07 21:27:36 +0900

    20190107 Morning TDD / Racing car - 기존 페어프로그래밍 테스트 케이스 설계 사용 - 전진값을_주어졌을떄_자동차의_현재_위치값이_변경구현

--- tddExercise/src/racingCar/RaceCar.java
+++ tddExercise/src/racingCar/RaceCar.java
@@ -2,6 +2,24 @@ package racingCar;
 
 public class RaceCar {
 
+    int currentCoord;
+
+    public RaceCar() {
+        this.currentCoord = 0;
+    }
+
+    public RaceCar(int currentCoord) {
+        this.currentCoord = currentCoord;
+    }
+
+    public int getCurrentCoord() {
+        return currentCoord;
+    }
+
+    public void goCar(int forwadNum) {
+        this.currentCoord += forwadNum;
+    }
+
 
 //    public int inputCarNum(int inputCarNum) {
 ////        List<Car> carList = new ArrayList<>();
--- tddExercise/test/racingCar/RaceCarTest.java
+++ tddExercise/test/racingCar/RaceCarTest.java
@@ -19,7 +19,19 @@ public class RaceCarTest {
     @Test
     public void 입력받은_차_수만큼_car_객체를_생성하는지_확인() {
         int inputCarNum = 3;
-        RacingGame racingGame = new RacingGame(3);
+        RacingGame racingGame = new RacingGame(inputCarNum);
         assertEquals(inputCarNum, racingGame.rc.size());
+    }
+
+    @Test
+    public void 전진값을_주어졌을떄_자동차의_현재_위치값이_변경됨() {
+        //  -  goCar(3) -> car.coord =+ 3
+        int forwadNum = 1;
+        int currentCoord = 3;
+
+        RaceCar rc = new RaceCar(currentCoord);
+
+        rc.goCar(forwadNum);
+        assertEquals(forwadNum+currentCoord, rc.getCurrentCoord());
     }
 }
